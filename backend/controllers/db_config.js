e115b7e52d0dbd1b8311178a3fb58ce4b8be5905
jakubmat2001 <jakubmatusik11@gmail.com>
2023-12-29 05:06:13 +0000

    added a docker container for backend

--- backend/controllers/db_config.js
+++ backend/controllers/db_config.js
@@ -3,8 +3,7 @@ const config = {
     port: process.env.PORT || 8000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     
-    mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
+    mongoUri: process.env.MONGO_URI ||
     'mongodb://' + (process.env.IP || '127.0.0.1') + ':' + (process.env.MONGO_PORT || '27017')+'/employeeDB/'
   }
 
